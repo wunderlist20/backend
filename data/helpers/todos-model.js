f24b74aee185de4bbb2500d9078fa2d517f28a17
harjothkhara <harjoth.khara@gmail.com>
2019-06-23 11:09:59 -0700

    knex cleaner added to cleaner seed. migration ran. todoRouter and todos-model created.

---
+++ data/helpers/todos-model.js
@@ -0,0 +1,54 @@
+const db = require('dbConfig.js');
+
+module.exports = {
+    find,
+    findById,
+    create,
+    remove,
+    update
+};
+
+async function find() {
+    const todos = await db("todos");
+    return todos;
+}
+
+async function findById(id) {
+    const todo = await db("todos")
+        .where({ id })
+        .first();
+    return todo;
+}
+
+async function create(item) {
+    const [id] = await db("todos")
+        .insert(item)
+        .returning("id");
+    if(id) {
+        const todo = await findById(id);
+        return todo;
+    }
+}
+
+async function remove(id) {
+    const todo = await findById(id);
+     if (todo) {
+         const deleted = await db("todos")
+            .where({ id })
+            .del();
+        if (deleted) {
+          return todo;
+        }
+     }
+}
+
+async function update(item, id) {
+    const editedTodo = await db("todos")
+        .where({ id })
+        .update(item);
+    if (editedTodo) {
+        const todo = await findById(id);
+        return todo;
+    }
+}
+
