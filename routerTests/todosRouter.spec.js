23a639ec020d9adb19c2b1ea0f2fa3de0b3f28be
harjothkhara <harjoth.khara@gmail.com>
2019-06-26 19:38:59 -0700

    testing changes

--- routerTests/todosRouter.spec.js
+++ routerTests/todosRouter.spec.js
@@ -3,15 +3,12 @@ const server = require('../api/server.js');
 const db = require('../data/dbConfig.js');
 
 const post = {
-    "id": 31,
-    "user_id": 31,
-    "title": "debugging with Andy",
-    "task": "backend",
-    "notes": "learned methodical debugging model",
+    "user_id": 1,
+    "title": "Jewelery",
+    "task": "Ergonomic Rubber Sausages",
+    "notes": "Quam sapiente natus ut ut cum. Voluptate esse modi. Voluptatum laboriosam nulla molestiae aperiam. Eius dolores rerum quas. Error est facere deleniti numquam non molestiae. Ut qui quos inventore dolorem ducimus sunt et dolorem blanditiis.",
     "setDate": "Tuesday",
-    "completed": true,
-    "created_at": "2019-06-26T04:10:48.925Z",
-    "updated_at": "2019-06-26T04:10:48.925Z"
+    "completed": 0,
 };
 
 describe("server.js", () => {
@@ -43,11 +40,19 @@ describe("GET /todos/:id", () => {
         expect(response.type).toBe("application/json");
     });
     it('should return requested object', async () => {
+        const post = {
+            "user_id": 1,
+            "title": "Jewelery",
+            "task": "Ergonomic Rubber Sausages",
+            "notes": "Quam sapiente natus ut ut cum. Voluptate esse modi. Voluptatum laboriosam nulla molestiae aperiam. Eius dolores rerum quas. Error est facere deleniti numquam non molestiae. Ut qui quos inventore dolorem ducimus sunt et dolorem blanditiis.",
+            "setDate": "Tuesday",
+            "completed": 0,
+        };
         await request(server)
         .post("/api/todos")
         .send(post);
-      const response = await request(server).get("/api/todos/31");
-      expect(response.body.title).toBe("debugging with Andy");
+      const response = await request(server).get("/api/todos/1");
+      expect(response.body.title).toBe("Jewelery");
     });
     it('should return status code 404 if object does not exist', async () => {
         const response = await request(server).get('/api/todos/40');
@@ -57,23 +62,47 @@ describe("GET /todos/:id", () => {
 
 describe("POST /todos", () => {
     it('should return status code 201 if successful', async () => {
+        const post = {
+            "user_id": 1,
+            "title": "Jewelery",
+            "task": "Ergonomic Rubber Sausages",
+            "notes": "Quam sapiente natus ut ut cum. Voluptate esse modi. Voluptatum laboriosam nulla molestiae aperiam. Eius dolores rerum quas. Error est facere deleniti numquam non molestiae. Ut qui quos inventore dolorem ducimus sunt et dolorem blanditiis.",
+            "setDate": "Tuesday",
+            "completed": 0,
+        };
         const response = await request(server)
             .post("/api/todos")
             .send(post);
         expect(response.status).toBe(201);
     });
     it('should return json if successful', async () => {
+        const post = {
+            "user_id": 1,
+            "title": "Jewelery",
+            "task": "Ergonomic Rubber Sausages",
+            "notes": "Quam sapiente natus ut ut cum. Voluptate esse modi. Voluptatum laboriosam nulla molestiae aperiam. Eius dolores rerum quas. Error est facere deleniti numquam non molestiae. Ut qui quos inventore dolorem ducimus sunt et dolorem blanditiis.",
+            "setDate": "Tuesday",
+            "completed": 0,
+        };
         const response = await request(server)
             .post("/api/todos")
             .send(post);
         expect(response.type).toBe("application/json");
     });
     it('should return created object if successful', async () => {
+        const post = {
+            "user_id": 1,
+            "title": "Jewelery",
+            "task": "Ergonomic Rubber Sausages",
+            "notes": "Quam sapiente natus ut ut cum. Voluptate esse modi. Voluptatum laboriosam nulla molestiae aperiam. Eius dolores rerum quas. Error est facere deleniti numquam non molestiae. Ut qui quos inventore dolorem ducimus sunt et dolorem blanditiis.",
+            "setDate": "Tuesday",
+            "completed": 0,
+        };
         const response = await request(server)
             .post("/api/todos")
             .send(post);
         expect(typeof response).toBe("object");
-        expect(response.body.title).toBe("debugging with Andy");
+        expect(response.body.title).toBe("Jewelery");
     });
     it('should fail with status code 404 if information is incomplete', async () => {
         const response = await request(server)
@@ -88,6 +117,14 @@ describe("POST /todos", () => {
 
 describe("DEL /todos/:id", () => {
     it("Should return status code 200 OK", async () => {
+        const post = {
+            "user_id": 1,
+            "title": "Jewelery",
+            "task": "Ergonomic Rubber Sausages",
+            "notes": "Quam sapiente natus ut ut cum. Voluptate esse modi. Voluptatum laboriosam nulla molestiae aperiam. Eius dolores rerum quas. Error est facere deleniti numquam non molestiae. Ut qui quos inventore dolorem ducimus sunt et dolorem blanditiis.",
+            "setDate": "Tuesday",
+            "completed": 0,
+        };
       await request(server)
         .post("/api/todos")
         .send(post);
@@ -95,6 +132,14 @@ describe("DEL /todos/:id", () => {
       expect(response.status).toBe(200);
     });
     it("should return json", async () => {
+        const post = {
+            "user_id": 1,
+            "title": "Jewelery",
+            "task": "Ergonomic Rubber Sausages",
+            "notes": "Quam sapiente natus ut ut cum. Voluptate esse modi. Voluptatum laboriosam nulla molestiae aperiam. Eius dolores rerum quas. Error est facere deleniti numquam non molestiae. Ut qui quos inventore dolorem ducimus sunt et dolorem blanditiis.",
+            "setDate": "Tuesday",
+            "completed": 0,
+        };
       await request(server)
         .post("/api/todos")
         .send(post);
@@ -102,6 +147,14 @@ describe("DEL /todos/:id", () => {
       expect(response.type).toBe("application/json");
     });
     it("should return deleted object", async () => {
+        const post = {
+            "user_id": 1,
+            "title": "Jewelery",
+            "task": "Ergonomic Rubber Sausages",
+            "notes": "Quam sapiente natus ut ut cum. Voluptate esse modi. Voluptatum laboriosam nulla molestiae aperiam. Eius dolores rerum quas. Error est facere deleniti numquam non molestiae. Ut qui quos inventore dolorem ducimus sunt et dolorem blanditiis.",
+            "setDate": "Tuesday",
+            "completed": 0,
+        };
       await request(server)
         .post("/api/todos")
         .send(post);
