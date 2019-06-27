b76f45c0394bc01c0856a822b635accdcf7caf97
harjothkhara <harjoth.khara@gmail.com>
2019-06-27 11:35:54 -0700

    authentication passed back into todos route; changed environment variable back to production from development

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,8 +1,8 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-// const dbEnvironment = process.env.NODE_ENV 
+const dbEnvironment = process.env.NODE_ENV 
 //for testing and running SQLite
-const dbEnvironment = process.env.DB_ENVIRONMENT || "development"; 
+// const dbEnvironment = process.env.DB_ENVIRONMENT || "development"; 
 
 module.exports = knex(config[dbEnvironment]);
